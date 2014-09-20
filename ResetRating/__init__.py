bab8d202a279333a690a7179c06ae077695ef06e
Steve Kemp <steve@steve.org.uk>
2014-09-20 14:45:08 +0100

    Added ResetRating plugin

---
+++ ResetRating/__init__.py
@@ -0,0 +1,26 @@
+from calibre.customize import FileTypePlugin
+
+class ResetRating(FileTypePlugin):
+
+    name                    = 'ResetRatings'
+    description             = 'When a book is imported set the rating to zero.'
+    supported_platforms     = ['windows', 'osx', 'linux']
+    author                  = 'Steve Kemp <steve@steve.org.uk>'
+    version                 = (0, 0, 2)
+    file_types              = set(['epub', 'mobi', 'lit', 'prc', 'azw3'])
+    on_postimport           = True
+    minimum_calibre_version = (2,3,0)
+
+    def run(self, path_to_ebook):
+        '''
+        NOP
+        '''
+        return path_to_ebook
+
+    def postimport(self, book_id, book_format, db):
+        '''
+        Once a book has been added to the database reset the rating field
+        '''
+        print( "RatingReset::postimport()")
+        print( "Current rating: %s" % db.new_api.field_for('rating', book_id))
+        db.new_api.set_field('rating', {book_id:0})
